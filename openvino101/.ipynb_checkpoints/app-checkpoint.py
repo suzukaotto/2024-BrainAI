94114edb01ed10eb7d16f1105acc00117cb04de3
suzukaotto <me@szk.kr>
2024-09-19 05:15:44 +0900

    update: video processing updated

--- openvino101/.ipynb_checkpoints/app-checkpoint.py
+++ openvino101/.ipynb_checkpoints/app-checkpoint.py
@@ -3,6 +3,22 @@ import PIL
 import cv2
 import numpy as np
 import utils
+import io
+
+def play_video(video_source):
+    camera = cv2.VideoCapture(video_source)
+
+    st_frame = st.empty()
+    while (camera.isOpened()):
+        ret, frame = camera.read()
+
+        if ret:
+            visualized_image = utils.predict_image(frame, conf_threshold)
+            st_frame.image(visualized_image, channels = "BGR")
+
+        else:
+            camera.release()
+            break
 
 st.set_page_config(
     page_title="Age/Gender/Emotion",
@@ -30,3 +46,22 @@ if source_radio == "IMAGE":
         visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
         
         st.image(visualized_image, channels = "BGR")
+
+temporary_location = None
+if source_radio == "VIDEO":
+    st.sidebar.header("Upload")
+    input = st.sidebar.file_uploader("Choose an video.", type=("mp4"))
+
+    if input is not None:
+        g = io.BytesIO(input.read())
+        temporary_location = "upload.mp4"
+
+        with open(temporary_location, "wb") as out:
+            out.write(g.read())
+
+        out.close()
+
+    if temporary_location is not None:
+        play_video(temporary_location)
+        if st.button("Replay", type="primary"):
+            pass
