361e7f17d9ea5f761083312bec44a84a039e3dd8
suzukaotto <me@szk.kr>
2024-09-19 05:03:29 +0900

    update: streamlit application confidence slider updated.

--- openvino101/.ipynb_checkpoints/app-checkpoint.py
+++ openvino101/.ipynb_checkpoints/app-checkpoint.py
@@ -16,6 +16,9 @@ st.title("Age/Gender/Emotion :sun_with_face:")
 st.sidebar.header("Type")
 source_radio = st.sidebar.radio("Select Source", ["IMAGE", "VIDEO", "WEBCAM"])
 
+st.sidebar.header("Confidence")
+conf_threshold = float(st.sidebar.slider("Select the Confidence Threshold", 10, 100, 20))/100
+
 input = None
 if source_radio == "IMAGE":
     st.sidebar.header("Upload")
@@ -24,6 +27,6 @@ if source_radio == "IMAGE":
     if input is not None:
         uploaded_image = PIL.Image.open(input)
         uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
-        visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = .2)
+        visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
         
         st.image(visualized_image, channels = "BGR")
