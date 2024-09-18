7d6f70c3af62b467391afc8b5ba1a76fc5cd32fc
suzukaotto <me@szk.kr>
2024-09-19 04:46:21 +0900

    update: streamlit file upload function added

--- openvino101/app.py
+++ openvino101/app.py
@@ -1,8 +1,23 @@
 import streamlit as st
+import PIL
 
 st.set_page_config(
     page_title="Age/Gender/Emotion",
-    page_icon="	:sun_with_face:",
+    page_icon=":sun_with_face:",
     layout="centered",
     initial_sidebar_state="expanded",
 )
+
+st.title("Age/Gender/Emotion :sun_with_face:")
+
+st.sidebar.header("Type")
+source_radio = st.sidebar.radio("Select Source", ["IMAGE", "VIDEO", "WEBCAM"])
+
+input = None
+if source_radio == "IMAGE":
+    st.sidebar.header("Upload")
+    input = st.sidebar.file_uploader("Choose an image.", type=("jpg", "png"))
+
+    if input is not None:
+        uploaded_image = PIL.Image.open(input)
+        st.image(uploaded_image)
